29a184e0ac47c863706b78754517b2495073e5cf
jieun <je_eun2735@naver.com>
2023-03-10 11:59:25 +0900

    틀 잡는 중

--- 정지은 - 20230310-javascript-exam-sample/source/modulefile.js
+++ 정지은 - 20230310-javascript-exam-sample/source/modulefile.js
@@ -58,41 +58,72 @@ root.appendChild(header);
 const main = document.createElement('main');
 root.appendChild(main);
 
-//main 안에서만 움직이는 박스1, 박스2
-const box1 =document.createElement('div');
-main.appendChild(box1);
-const box2 =document.createElement('div');
-main.appendChild(box2);
 const footer = document.createElement('footer');
 root.appendChild(footer);
+
+const names = document.createElement('div');
+main.appendChild(names);
+const container = document.createElement('div');
+main.appendChild(container);
+const img = document.createElement('div');
+container.appendChild(img);
+const text = document.createElement('div');
+container.appendChild(text);;
+
+container.setAttribute('id','container');
+names.setAttribute('id','names');
+img.setAttribute('id','img');
+text.setAttribute('id','text');
+main.setAttribute('id','main');
 
 //-------------------------------
 root.style.width ='768px';
 root.style.height ='200vh';
 
-header.style.backgroundColor='#333';
+header.style.backgroundColor='#ccc';
 header.style.width ='100%';
 header.style.height ='10vh';
+header.textContent = "제목을 뭘로 할까? ";
 
 main.style.backgroundColor='cadetblue';
 main.style.width ='100%';
 main.style.height='70%';
 
+container.style.display='flex';
+container.style.width ='100%';
+container.style.height='45%';
+
+
+names.textContent='공욱재';
+img.textContent='사진넣을거임';
+text.textContent='텍스트넣을거임';
+
+names.style.width ='100%';
+names.style.height='5%';
+
+img.style.width ='40%';
+img.style.height='100%';
+
+text.style.width ='60%';
+text.style.height='100%';
+
 footer.style.backgroundColor='salmon';
 footer.style.width ='100%';
-footer.style.height='30%';
+footer.style.height='20%';
 
-box1.style.backgroundColor='#ccc';
+
+//---------//main 안에서만 움직이는 박스1, 박스2
+
+const box1 =document.createElement('div');
+main.appendChild(box1);
+
+box1.style.backgroundColor='pink';
 box1.style.width = '100%';
-box1.style.height ='40%';
+box1.style.height ='70%';
 box1.style.position = 'absolute';
-box1.style.top = '300px';
+box1.style.top = '1050px';
 
-box2.style.backgroundColor='pink';
-box2.style.width = '100%';
-box2.style.height ='40%';
-box2.style.position = 'absolute';
-box2.style.top = '700px';
+
 
 
 
